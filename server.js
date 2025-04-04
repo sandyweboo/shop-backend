159f1e548daf820bcf051da4e7950a2177daa046
Sandun Gunawardana <102416086+sandyweboo@users.noreply.github.com>
2025-04-04 23:22:59 +0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+ 
+const app = express();
+
+
+
+app.listen(3000);
