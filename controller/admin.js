ef2100176373c4c9091b90829e6c20f58120e468
Sandun Gunawardana <102416086+sandyweboo@users.noreply.github.com>
2025-04-16 21:56:03 +0400

    Refactor product management: implement edit and view product features, update navbar, and remove deprecated add product view

--- controller/admin.js
+++ controller/admin.js
@@ -1,18 +1,44 @@
 const Product = require('../models/product')
 
-exports.getAddProduct = (req, res, next)=>{
-res.render('admin/addProduct')
+exports.getAddProduct = (req, res, next) => {
+    res.render('admin/edit-Product')
 }
 
-exports.postAddProduct = (req, res, next)=>{
+
+exports.postAddProduct = (req, res, next) => {
     const id = Math.floor(Math.random() * 1e10);
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
 
     console.log(title, imageUrl, price)
-const newProduct  = new Product(id,title, imageUrl, price)
-newProduct.save();
-   // res.render('admin/addProduct')
+    const newProduct = new Product(id, title, imageUrl, price)
+    newProduct.save();
+    // res.render('admin/addProduct')
+}
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit === "true";
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
     }
-    
+    res.render('admin/edit-Product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-Product',
+      editing: editMode,
+      product: product
+    });
+  });
+
+} 
+
+exports.getProduct = (req, res, next) =>{
+    Product.fetchAll(products =>{
+        res.render('admin/adminProduct',{prods : products})
+    })
+}
+
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const adminController = require('../controller/admin');
-const router = express.Router(); 
+const router = express.Router();
 
-router.get('/add-product',adminController.getAddProduct);
-router.post('/add-product',adminController.postAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+router.post('/add-product', adminController.postAddProduct);
+router.get('/edit-product', adminController.getEditProduct);
+router.get('/get-product', adminController.getProduct)
 module.exports = router;
